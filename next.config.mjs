fb7e3cfe8e528bf2b6ba7aa8784d9475d0af35ea
Wallace do Vale <dovalewallace@gmail.com>
2026-01-24 11:07:53 -0300

    restore deposit modal with manual check button

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+import withPWA from 'next-pwa';
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})(nextConfig);
