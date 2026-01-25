baab1bf600dacda7b0ac7f7e48213b993ab14a80
Wallace do Vale <dovalewallace@gmail.com>
2026-01-25 20:22:59 -0300

    Adicionar musica de fundo e corrigir tela branca PC

--- app/page.tsx
+++ app/page.tsx
@@ -558,4 +558,4 @@ export default function Home() {
       <ProfileSidebar isOpen={isProfileOpen} onClose={() => setIsProfileOpen(false)} user={user} balance={balance} onLogout={handleLogout} />
     </>
   );
-}
+} 
