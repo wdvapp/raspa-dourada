510aeb98c4f424fac3395ff0ec01c2f0e63e3f93
Wallace do Vale <dovalewallace@gmail.com>
2026-01-25 15:12:45 -0300

    fix config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Ignora erros de TypeScript
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Ignora erros de Lint
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
