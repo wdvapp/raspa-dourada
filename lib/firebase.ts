7433b5faf5541a5b89390dca4386153120402615
Wallace do Vale <dovalewallace@gmail.com>
2026-01-26 17:32:34 -0300

    Corrigindo build vercel

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,19 +1,30 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: "raspa-dourada.appspot.com",
-  // COLOQUE OS NÚMEROS REAIS AQUI EMBAIXO (Tire o "seu_id_aqui"):
-  messagingSenderId: "1035486868104", 
-  appId: "1:1035486868104:web:ef76b9e56ac89825cde53d"
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: "1035486868104", // ID do seu projeto que vi no print
+  appId: "1:1035486868104:web:ef76b9e56ac89825cde53d" // ID do app que vi no print
 };
 
+// Inicia o App (Singleton para evitar recriar)
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { app, db, auth };
+// Inicia o Messaging (apenas no navegador, pois no servidor não existe 'window')
+let messaging: any = null;
+if (typeof window !== "undefined") {
+  try {
+    messaging = getMessaging(app);
+  } catch (err) {
+    console.error("Firebase Messaging não suportado neste navegador.", err);
+  }
+}
+
+export { app, db, auth, messaging };
